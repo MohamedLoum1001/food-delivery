1382c0e0a0c22a6127f0f683ddf070bedb3da8bf
MohamedLoum1001 <mloum137@gmail.com>
2023-10-09 18:07:14 +0000

    modif

---
+++ Data/Data.js
@@ -0,0 +1,35 @@
+
+export const navlink = [
+    {
+        id: 1,
+        lien: 'Food'
+    },
+    {
+        id: 2,
+        lien: 'Drinks'
+    }, 
+    {
+        id: 3,
+        lien: 'Snacks'
+    }, 
+    {
+        id: 4,
+        lien: 'Sauce'
+    }
+]
+
+export const cardImage = [
+    {
+        img : require('../assets/images/plat1.png'),
+        titre: "Veggie",
+        texte: "tomato mix",
+        prix: "N1,900"
+    },
+    {
+
+        img : require('../assets/images/plat2.png'),
+        titre: "Spicy",
+        texte: "fish sauce",
+        prix: "N2,300.99"
+    },
+]
---
+++ components/CardMenu/CardMenu.jsx
@@ -0,0 +1,93 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Image, Text } from 'react-native';
+import { cardImage } from '../../Data/Data';
+
+export default function CardMenu() {
+    return (
+        <View style={styles.container}>
+            {cardImage.map(item => (
+                <View style={styles.card}>
+                    <View style={styles.cardHeader}>
+                        <Image source={item.img} style={styles.img} />
+                    </View>
+
+                    <View style={styles.title}>
+                        <Text style={styles.titre}>{item.titre}</Text>
+                        <Text style={styles.texte}>{item.texte}</Text>
+                    </View>
+                    
+                    <View style={styles.viewPrix}>
+                        <Text style={styles.prix}>{item.prix}</Text>
+                    </View>
+                </View>
+            ))}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f2f2f2',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    card: {
+        width: 170,
+        height: 250,
+        backgroundColor: '#fff',
+        elevation: 5,
+        borderRadius: 30,
+        position: 'relative',
+        zIndex: 1,
+        marginLeft: 70,
+        marginTop: 50,
+        // padding: 20,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: -350,
+        position: 'relative',
+        zIndex: 1
+    },
+    cardHeader: {
+        elevation: 5,
+        position: 'absolute',
+        zIndex: 300,
+        paddingTop: 20
+    },
+    img: {
+        borderRadius: 30,
+        marginTop: -170,
+        width: 100,
+        height: 100,
+       
+        display: 'flex'
+    },
+    title: {
+        // marginTop: 70,
+        paddingTop:  20
+    },
+    titre: {
+        fontWeight: '600',
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    texte: {
+        fontWeight: '600',
+        // padding: 20,
+        fontSize: 22,
+        textAlign: 'center'
+    },
+viewPrix: {
+    marginTop: 20,
+},
+    prix: {
+        color: '#FA4A0C',
+        fontSize: 17,
+        fontWeight: '700',
+        fontStyle: 'normal',
+        textAlign: 'center'
+    }
+});
